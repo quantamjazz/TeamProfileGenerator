39954b2cc7a248588a1db0f6470ce6e2f07eba3d
quantamjazz <148623614+quantamjazz@users.noreply.github.com>
2024-02-13 21:12:24 +0000

    Updated Engineer and Manager classes to inherit from Employee class

--- starter code/starter/lib/Engineer.js
+++ starter code/starter/lib/Engineer.js
@@ -1,1 +1,25 @@
-// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+// engineer.js
+
+// Import the Employee class
+const Employee = require('./Employee');
+
+class Engineer extends Employee {
+
+  constructor(name, id, email, github) {
+
+    super(name, id, email);
+
+    this.github = github;
+  }
+
+  getGithub() {
+    return this.github;
+  }
+
+  getRole() {
+    return 'Engineer';
+  }
+}
+
+// Export the Engineer class
+module.exports = Engineer;
